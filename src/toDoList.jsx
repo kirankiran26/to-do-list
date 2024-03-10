79c04790dea64be771ac49774dea342cc6350767
Kiran P S <kiranps2607@gmail.com>
2024-03-10 16:42:04 +0530

    Added addtask and delet task fun

--- src/toDoList.jsx
+++ src/toDoList.jsx
@@ -7,22 +7,39 @@ function List() {
   function handleNewTask(event) {
     setNewTask(event.target.value);
   }
+  const addtask = () => {
+    if(newTask.trim()!=="") {
+    setTasks((t)=>[...t,newTask]);
+    setNewTask("")
+    }
+    
+  };
+  const emptydiv=document.getElementById("notasktocom")
+  const deletebtn= (index) => {
+    const updatedlist=tasks.filter((_,i)=>index!==i);
+    if (updatedlist.length===0) {
+      emptydiv.innerText="Done for the day"
+    }
+    setTasks(updatedlist)
+  }
 
   return (
     <div id="todolist">
     <div >
       <h1>TO-DO-LIST</h1>
+      <h1 id='notasktocom'></h1>
       <ol >
         {tasks.map((task,index)=> 
         <li key={index}><b>{task}</b>
-        <button>Delete </button>
+        <button onClick={()=>{deletebtn(index)}}>Delete </button>
         <button id='moveup'>👆 </button>
         <button id='movedown'> 👇</button>
         </li>)}
       </ol>
      <div className="inpbtttn">
-        <input type="text" placeholder='Enter the new task' />
-        <button id='addbttn'> Add </button>
+        <input type="text" placeholder='Enter the new task' required value={newTask} onChange={handleNewTask} />
+        <button id='addbttn' onClick={addtask}> Add </button>
+        
      </div>
     </div>
     </div>
